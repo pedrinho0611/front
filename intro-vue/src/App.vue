b958777106268848140fe34774d1ca1487c1c343
Pedro Rodrigues <173830932+pedrinho0611@users.noreply.github.com>
2025-08-13 07:27:42 -0300

    novas alterações

--- intro-vue/src/App.vue
+++ intro-vue/src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
-import Exemplo01 from './components/exercicios/exemplo01exercicio.vue'
-import Exemplo02 from './components/exercicios/Exemplo02exercicio.vue'
-import Exemplo03 from './components/exercicios/Exemplo03exercicio.vue'
+// import Exemplo01 from './components/exercicios/exemplo01exercicio.vue'
+// import Exemplo02 from './components/exercicios/Exemplo02exercicio.vue'
+// import Exemplo03 from './components/exercicios/Exemplo03exercicio.vue'
+import Exemplo04 from './components/exercicios/Exemplo04exercicio.vue'
 </script>
 
 <template>
   <Exemplo01></Exemplo01> 
   <Exemplo02></Exemplo02>
   <Exemplo03></Exemplo03>
+  <Exemplo04></Exemplo04>
 
 </template>
---
+++ intro-vue/src/components/exercicios/Exemplo04exercicio.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<p v-if="usuarioLogado">Bem-vindo</p>
+		<p v-else>Faça login</p>
+		<button @click="alternarLogin">
+			{{ usuarioLogado ? 'Logout' : 'Login' }}
+		</button>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			usuarioLogado: false
+		}
+	},
+	methods: {
+		alternarLogin() {
+			this.usuarioLogado = !this.usuarioLogado;
+		}
+	}
+}
+</script>
