6f427dbacfa526a204ba415da17a6870467fbb49
Pedro Rodrigues <173830932+pedrinho0611@users.noreply.github.com>
2025-08-29 07:16:31 -0300

    n

---
+++ intro-vue/src/components/Exemplo09Emits.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref} from 'vue'
+
+const emit = defineEmits(['update'])
+const count = ref(0)
+
+function Incrementar() {
+    count.value++
+    emit('update', count.value)
+}
+</script>
+
+<template>
+    <div>
+        <p> Contador:{{ count }}</p>
+        <button @click="Incrementar">+1</button>
+    </div>
+</template>
+
+<style scoped>
+</style>
